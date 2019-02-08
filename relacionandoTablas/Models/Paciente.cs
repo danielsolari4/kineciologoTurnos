8dc732281f39d170833fa0aa7b182cfe5d8417b2
danielsolari451 <danielsolari451@hotmail.com>
2019-02-08 16:01:00 -0300

    61%

--- relacionandoTablas/Models/Paciente.cs
+++ relacionandoTablas/Models/Paciente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,15 @@ namespace relacionandoTablas.Models
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        [Display(Name ="Fecha de Nacimiento")]
+        public int Edad { get; set; }
+        public int Telefono { get; set; }
+        public string Dirección { get; set; }
+        public string ObraSocial { get; set; }
+        public string HistoriaMedica { get; set; }
+        public string Correo { get; set; }
+
         public virtual List<Turno> Turnos { get; set; }
     }
 }
